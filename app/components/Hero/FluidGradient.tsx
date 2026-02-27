aa9d46f2a4771d0926b8ac1e47dab862a0e50a33
migeles <kikjidetes@gmail.com>
2026-02-28 00:02:03 +0700

    remove bouncing profile pic

--- app/components/Hero/FluidGradient.tsx
+++ app/components/Hero/FluidGradient.tsx
@@ -228,7 +228,7 @@ export default function FluidGradient({ className = "" }: FluidGradientProps) {
         <GradientBackgroundMesh />
 
         {/* Layer 2: The foreground bouncing profile picture */}
-        <BouncingProfilePic />
+        {/* <BouncingProfilePic /> */}
       </Canvas>
     </div>
   );
